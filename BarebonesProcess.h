9ef21575ba2c70390376ed76bc5dbc9c0ca4d1fe
Ian <115538235+IanRein@users.noreply.github.com>
2025-06-15 23:59:58 +0800

    Update BarebonesProcess.h

--- BarebonesProcess.h
+++ BarebonesProcess.h
@@ -1,15 +1,7 @@
-
 #ifndef BAREBONES_PROCESS_H
 #define BAREBONES_PROCESS_H
 
 #include <string>
-
-struct Process {
-    std::string name;
-    int currentLine;
-    int totalLines;
-    std::string timestamp;
-};
 
 void createBarebonesProcess(const std::string& name);
 
